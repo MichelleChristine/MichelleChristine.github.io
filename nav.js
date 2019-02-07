904482db3c61e1765c4326f75ecda515441fc905
MichelleChristine <mcb35@uw.edu>
2019-02-06 19:06:02 -0800

    new tested website

---
+++ nav.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('header').load('head.html');
+    $('footer').load('footer.html');
+    // $('footer').append('<button onclick="topFunction()" id="topBtn">Top</button>')
+});
+
+$(document).ready(function () {
+
+    // $('#home').css('text-decoration', 'underline');
+    //for each of the navigation elements:
+    // var navItems = $('nav ul li');
+
+    // function addClickListener(items) {
+    //     items.array.forEach(element => {
+    //         element.click(function () {
+    //             $('#bodyContent').load(element.id + '.html');
+    //         })
+    //     });
+    // }
+
+    // addClickListener($('nav ul li'));
+});
