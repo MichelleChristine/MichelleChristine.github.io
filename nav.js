7c4236db715c1aa7d4794765800709cdeca74c4e
MichelleChristine <mcb35@uw.edu>
2019-02-08 11:37:37 -0800

    added TOP button

--- nav.js
+++ nav.js
@@ -1,22 +1,24 @@
 $(function() {
     $('header').load('head.html');
-    $('footer').load('footer.html');
-    // $('footer').append('<button onclick="topFunction()" id="topBtn">Top</button>')
+    $('#footerDiv').load('footer.html');
+    $('footer').append('<button onclick="topFunction()" id="topBtn">TOP</button>');
 });
 
-$(document).ready(function () {
+$(document).ready(function () {});
 
-    // $('#home').css('text-decoration', 'underline');
-    //for each of the navigation elements:
-    // var navItems = $('nav ul li');
 
-    // function addClickListener(items) {
-    //     items.array.forEach(element => {
-    //         element.click(function () {
-    //             $('#bodyContent').load(element.id + '.html');
-    //         })
-    //     });
-    // }
+window.onscroll = function() {scrollFunction()};
 
-    // addClickListener($('nav ul li'));
-});
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    $('#topBtn').css('display', 'block');
+  } else {
+    $('#topBtn').css('display', 'none');
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
